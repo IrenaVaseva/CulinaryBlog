3ebcf83d846f9a9fc94fd5590348674f45f46354
IrenaVaseva <ir_vaseva@abv.bg>
2024-12-04 19:49:33 +0200

    Init

---
+++ scripts/menu.js
@@ -0,0 +1,24 @@
+function OpenLinks(str) {
+    if (str == "home") {
+        document.getElementById("Links").setAttribute("src", "../pages/home-page.html");
+    }
+
+    if (str == "salads") {
+        document.getElementById("Links").setAttribute("src", "");
+    }
+
+    if (str == "main-meal") {
+        document.getElementById("Links").setAttribute("src", "");
+    }
+
+    if (str == "baked") {
+        document.getElementById("Links").setAttribute("src", "");
+    }
+    if (str == "desserts") {
+        document.getElementById("Links").setAttribute("src", "");
+    }
+
+    if (str == "about") {
+        document.getElementById("Links").setAttribute("src", "../pages/about.html");
+    }
+}
