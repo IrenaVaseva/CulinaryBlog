48f79a1b01fce9f3a147149c7fdaa4c82b71d3d6
IrenaVaseva <ir_vaseva@abv.bg>
2025-01-13 23:17:40 +0200

    add menu icon

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,7 @@
+const menuIcon = document.getElementById("menuIcon");
+const menuNav = document.getElementById("navMenu");
+const imgMenu=document.getElementById("imgMenu");
+
 function OpenLinks(str) {
     if (str == "home") {
         document.getElementById("Links").setAttribute("src", "./pages/home-page.html");
@@ -22,3 +26,15 @@ function OpenLinks(str) {
         document.getElementById("Links").setAttribute("src", "./pages/about.html");
     }
 }
+
+/* ------------------ */
+menuIcon.addEventListener('click', (e) => {
+    if (menuNav.style.display == 'none') {
+        menuNav.style.display = 'flex';
+        imgMenu.src = "./images/close_icon.svg";
+    } else {
+        menuNav.style.display = 'none';
+        imgMenu.src = "./images/menu_icon.svg";
+    }
+
+})
